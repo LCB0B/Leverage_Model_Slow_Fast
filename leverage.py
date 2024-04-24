9b6182edfd8652b86bf3b4dd1bc778c376124459
Louis_B <louisboucherie@protonmail.com>
2024-04-24 22:59:01 +0200

    revive repo

---
+++ leverage.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def discrete_system():
+    return
+    
+def continous_system():
+    #find something for the case 0 where the stable instasbility is push to infinity
+    return
+
+def stochastic_system():
+    #type of noise as an inout
+    return
+
+# get cycle time distribution 
